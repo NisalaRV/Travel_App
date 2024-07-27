b23020b27ed076234dce59902950020cd1348f56
Nisala Ravihara <116428103+NisalaRV@users.noreply.github.com>
2024-07-27 13:42:47 +0530

    setup Project

---
+++ data/categories.ts
@@ -0,0 +1,44 @@
+const destinationCategories = [
+  {
+    title: "All",
+    iconName: ""
+  },
+  {
+    title: "Central",
+    iconName: ""
+  },
+  {
+    title: " Southern",
+    iconName: ""
+  },
+  {
+    title: "Eastern",
+    iconName: ""
+  },
+  {
+    title: "North Central",
+    iconName: ""
+  },
+  {
+    title: "Northern",
+    iconName: ""
+  },
+  {
+    title: " North Western",
+    iconName: ""
+  },
+  {
+    title: " Sabaragamuwa ",
+    iconName: ""
+  },
+  {
+    title: " Uva",
+    iconName: ""
+  },
+  {
+    title: "Western",
+    iconName: ""
+  }
+];
+
+export default destinationCategories;
